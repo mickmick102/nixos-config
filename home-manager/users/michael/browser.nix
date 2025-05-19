eae0c915666a9c494ebc35c9884e57190f8ebf61
mickmick102 <michaelc1998@hotmail.ca>
2025-05-18 23:43:07 -0400

    test 2

--- home-manager/users/michael/browser.nix
+++ home-manager/users/michael/browser.nix
@@ -12,50 +12,50 @@
       { id = "mcbpblocgmgfnpjjppndjkmgjaogfceg"; }
     ];
   };
-  
+
   programs.firefox = {
     enable = true;
-    profiles = {
-      default = {
-        id = 0;
-        name = "default";
-        isDefault = true;
-        settings = {
-          "browser.startup.homepage" = "https://google.ca";
-          "browser.search.defaultenginename" = "Google";
-          "browser.search.order.1" = "Google";
-        };
-        search = {
-          force = true;
-          default = "Google";
-          order = [ "Google" ];
-          engines = {
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "type"; value = "packages"; }
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
-            "NixOS Wiki" = {
-              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = [ "@nw" ];
-            };
-            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
-          };
-        };
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-          bitwarden
-          darkreader
-        ];
-      };
-    };
+    # profiles = {
+    #   default = {
+    #     id = 0;
+    #     name = "default";
+    #     isDefault = true;
+    #     settings = {
+    #       "browser.startup.homepage" = "https://google.ca";
+    #       "browser.search.defaultenginename" = "Google";
+    #       "browser.search.order.1" = "Google";
+    #     };
+    #     search = {
+    #       force = true;
+    #       default = "Google";
+    #       order = [ "Google" ];
+    #       engines = {
+    #         "Nix Packages" = {
+    #           urls = [{
+    #             template = "https://search.nixos.org/packages";
+    #             params = [
+    #               { name = "type"; value = "packages"; }
+    #               { name = "query"; value = "{searchTerms}"; }
+    #             ];
+    #           }];
+    #           icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+    #           definedAliases = [ "@np" ];
+    #         };
+    #         "NixOS Wiki" = {
+    #           urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+    #           iconUpdateURL = "https://nixos.wiki/favicon.png";
+    #           updateInterval = 24 * 60 * 60 * 1000; # every day
+    #           definedAliases = [ "@nw" ];
+    #         };
+    #         "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+    #       };
+    #     };
+    #     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    #       ublock-origin
+    #       bitwarden
+    #       darkreader
+    #     ];
+    #   };
+    # };
   };
 }
