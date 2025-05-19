6c52429034fbe93bf967874983afe66e1a56e95b
mickmick102 <michaelc1998@hotmail.ca>
2025-05-18 23:59:55 -0400

    test 9

--- home-manager/users/michael/default.nix
+++ home-manager/users/michael/default.nix
@@ -2,13 +2,13 @@
   imports = [
     ../../common/home.nix
     ./user.nix
-    #./git.nix
-    #./lazyvim.nix
-    #./alacritty.nix
-    #./browser.nix
-    #./starship.nix
-    #./tmux.nix
-    #./zsh.nix
-    #./qt.nix
+    ./git.nix
+    ./lazyvim.nix
+    ./alacritty.nix
+    ./browser.nix
+    ./starship.nix
+    ./tmux.nix
+    ./zsh.nix
+    ./qt.nix
   ];
 }
