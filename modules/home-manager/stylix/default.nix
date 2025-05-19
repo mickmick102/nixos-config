3db48a1b84cfebe1af31e8a51fb015012aad2bf2
mickmick102 <michaelc1998@hotmail.ca>
2025-05-18 23:41:12 -0400

    test

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
-  stylix = {
-    enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
-    image = ./wallpaper.png;
-  };
+  # stylix = {
+  #   enable = true;
+  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
+  #   image = ./wallpaper.png;
+  # };
 }
